3f81ea3e1f277311e304e6715c1f5d7884119e71
Ambroz <killerbee8@gmail.com>
2021-01-25 18:45:48 +0100

    czyszczenie

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -27,13 +27,10 @@ class MainController extends AbstractController
 
         $stationData = $apiData->getApiData();
 
-//$a = 0;
         foreach($stationData as $city){
-//            if($a != 6) {
+
                 $main_arr[$city['city']['name']] = $city['city']['id'];
                 $dupli_arr[$city['city']['id']][] = $city;
-//                $a++;
-//            }
         }
 
         $form = $this->createFormBuilder()
@@ -56,8 +53,6 @@ class MainController extends AbstractController
             $id = $form->getData()['city'];
             $stationId = $dupli_arr[$id];
 
-//            dump($dupli_arr);
-
             foreach($stationId as $id){
 
                 $stationDetails[$id['id']] = array(
@@ -73,26 +68,12 @@ class MainController extends AbstractController
                 $stationsData [] = $apiData->getApiData($id['id']);
             }
 
-//        $sensorsData = $apiData->getApiData($sensorId);
-
-//        dump($stationsData);die;
-
             foreach($stationsData as $station){
-//                dump($station);die;
-
 
                 foreach($station as $data){
-//                    $data_arr[$data['stationId']][] = $data['param'];
                     $data_arr[$data['stationId']]['whatMeasure'][] = $data['param'];
                 }
-//            die;
-//            $station_arr[$station['stationId']][] = $station['id'];
             }
-
-            // Lista Stanowisk pomiarowych w stacjach
-//            dump($stationDetails);
-//            dump($stationDetailsstationId']);
-//            dump($data_arr);
 
             $data = array_replace_recursive($stationDetails, $data_arr);
 
--- src/Services/ApiData.php
+++ src/Services/ApiData.php
@@ -28,22 +28,18 @@ class ApiData
 
         }
 
-//        if($sensorId != null) {
-//            $url = 'http://api.gios.gov.pl/pjp-api/rest/data/getData/'.$sensorId;
-//        }
         $response = $this->client->request(
             'GET',
             $url
         );
 
         $statusCode = $response->getStatusCode();
-        // $statusCode = 200
+
         $contentType = $response->getHeaders()['content-type'][0];
-        // $contentType = 'application/json'
+
         $content = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+
         $content = $response->toArray();
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
 
         return $content;
 
